d3f81c3d2860a47b339098fc4b2cb7126719097e
adam-zaghi <123177300+adam-zaghi@users.noreply.github.com>
2026-07-15 13:12:25 +0100

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        return dfs(0,nums,0);
+        
+    }
+    public int dfs(int i,int[] nums, int total){
+
+        if(i>=nums.length){
+            return total;
+        }
+        return dfs(i+1,nums,total^nums[i])+dfs(i+1,nums,total);
+    }
+
+}
